abc15ace350f1d21f5a66cba58c8f23291302de6
MakiNaruto <become006@gmail.com>
2024-04-28 11:14:09 +0800

    增加注释信息

--- xml_operation.py
+++ xml_operation.py
@@ -6,13 +6,13 @@ __Created__ = "2024/02/01"
 __Description__ = "xml 节点树的处理方法"
 """
 
-from typing import List
 from copy import deepcopy
 from collections import deque
-from xml.dom.minidom import Document
+from xml.dom.minidom import Element
+from typing import List, Set, Union
 
 
-def get_node_by_path(node, path: str) -> List:
+def get_node_by_path(node: Element, path: str) -> List[Element]:
     """
     对当前的节点进行路径查找, 通过指定路径进行子树的搜索。
     @param node: minidom解析的树型节点。
@@ -33,7 +33,7 @@ def get_node_by_path(node, path: str) -> List:
     return res
 
 
-def get_single_node_by_path(node, path: str, return_value: bool = False) -> Document:
+def get_single_node_by_path(node: Element, path: str, return_value: bool = False) -> Union[str, Element]:
     nodes = get_node_by_path(node, path)
     if len(nodes) == 1:
         leaf = nodes[0]
@@ -44,7 +44,7 @@ def get_single_node_by_path(node, path: str, return_value: bool = False) -> Docu
             return leaf
 
 
-def get_children(node):
+def get_children(node: Element) -> List[Element]:
     """获取除叶子节外点的子节点"""
     nodes = list()
     for child in node.childNodes:
@@ -53,59 +53,69 @@ def get_children(node):
     return nodes
 
 
-def get_node_children_tags(node):
+def get_node_children_tags(node: Element) -> Set[str]:
     """ 获取节点所有的儿子节点标签名称。 """
     return {sub_node.nodeName for sub_node in get_children(node)}
 
 
-def get_node_value(node):
+def get_node_value(node: Element) -> str:
     """ 获取节点存储的文本值 """
     leaf_node = [3, 4, 10, 12]
     if node.nodeType in leaf_node:
         return node.nodeValue
 
 
-def search_node_by_name(root, search_list: list):
+def is_parenthesized(node: Element) -> bool:
+    res = get_single_node_by_path(node, f'{node.nodeName}/extra/parenthesized/#text', return_value=True)
+    return True if res == 'True' else False
+
+
+def search_node_by_name(node: Element, search_list: list) -> List[Element]:
     """
     深度搜索获取特定节点类型的节点。
-    @param root: minidom解析的树型根节点。
+    @param node: minidom解析的树型根节点。
     @param search_list: 搜索节点或属性名称
     """
     search_result = []
-    q = [root]
+    q = [node]
     while len(q):
-        node = q.pop(0)
-        if node.nodeName in search_list:
-            search_result.append(node)
+        sub_node = q.pop(0)
+        if sub_node.nodeName in search_list:
+            search_result.append(sub_node)
         else:
-            for child in get_children(node):
+            for child in get_children(sub_node):
                 q.append(child)
     return search_result
 
 
-def search_node_by_attribute(root, search_list: list, attribute_name: str = None):
+def search_node_by_attribute(node: Element, search_list: list, attribute_name: str = None) -> List[Element]:
     """
     深度搜索获取特定节点类型的节点。
-    @param root: minidom解析的树型根节点。
+    @param node: minidom解析的树型根节点。
     @param search_list: 搜索节点或属性名称
     @param attribute_name: 搜索的属性名称
     """
     search_result = []
-    q = [root]
+    q = [node]
     while len(q):
-        node = q.pop(0)
-        if node.getAttribute(attribute_name) in search_list:
-            search_result.append(node)
+        sub_node = q.pop(0)
+        if sub_node.getAttribute(attribute_name) in search_list:
+            search_result.append(sub_node)
         else:
-            for child in get_children(node):
+            for child in get_children(sub_node):
                 q.append(child)
     return search_result
 
 
-def replace_clone_node(ori_node, new_node, old_node):
-    """ 克隆当前节点, 并将新节点替换老节点 """
+def replace_clone_node(parent_node: Element, new_node: Element, old_node: Element) -> Element:
+    """
+    将当前节点的孩子节点进行替换
+    @param parent_node: 当前操作节点
+    @param new_node: 用于替换的节点
+    @param old_node: 将被替换的旧节点
+    """
     new_node.nodeName = old_node.nodeName
-    ori_node = ori_node.cloneNode(deep=True)
+    ori_node = parent_node.cloneNode(deep=True)
     old_node = get_single_node_by_path(ori_node, f'{ori_node.nodeName}/{old_node.nodeName}')
     new_node = new_node.cloneNode(deep=True)
     new_node.nodeName = old_node.nodeName
@@ -113,7 +123,7 @@ def replace_clone_node(ori_node, new_node, old_node):
     return ori_node
 
 
-def single_level_assignment_conditional_convert(assignment_node):
+def single_level_assignment_conditional_convert(assignment_node: Element) -> List[Element]:
     """
         转换赋值类型的三目表达式, 将赋值内容向下一层级转移例如:
 
@@ -125,8 +135,6 @@ def single_level_assignment_conditional_convert(assignment_node):
     ae_parent_node = assignment_node.parentNode
     ae_node = assignment_node
 
-    # ae_left = get_single_node_by_path(ae_node, f'{ae_node.nodeName}/left')
-    # ConditionalExpression node
     ce_node = get_single_node_by_path(ae_node, f'{ae_node.nodeName}/right')
     ce_consequent = get_single_node_by_path(ce_node, f'{ce_node.nodeName}/consequent')
     ce_alternate = get_single_node_by_path(ce_node, f'{ce_node.nodeName}/alternate')
@@ -140,16 +148,13 @@ def single_level_assignment_conditional_convert(assignment_node):
         ce_node.replaceChild(new_ae_replace_left, ce_consequent)
         ce_node.replaceChild(new_ae_replace_right, ce_alternate)
         ce_node.nodeName = ae_node.nodeName
-        # print(ae_node.nodeName, ce_consequent.nodeName, ce_node.nodeName)
 
-        # print('替换前: ', get_node_children_tags(ae_parent_node))
         ae_parent_node.replaceChild(ce_node, ae_node)
-        # print('替换后: ', get_node_children_tags(ae_parent_node))
         sub_ae_nodes = get_node_by_path(ae_parent_node, f'{ae_parent_node.nodeName}/{ce_node.nodeName}')
         return sub_ae_nodes
 
 
-def assignment_conditional_convert(root):
+def assignment_conditional_convert(root: Element):
     """
         转换赋值类型的三目表达式, 将赋值内容向下转移, 直至底层。例如:
 
